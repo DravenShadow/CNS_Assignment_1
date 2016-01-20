a86dbdd87bf1af7e6f1fd3ebfb7471fc74fc06b7
DravenPredator <depree.rowland@gmail.com>
2016-01-20 08:49:05 -0500

    Creation of more modules, some updates

--- Server_Tic_Tac_Toe.py
+++ Server_Tic_Tac_Toe.py
@@ -6,10 +6,16 @@ s = socket.socket()  # Create a socket object
 host = socket.gethostname()  # Get local machine name
 port = 12345  # Reserve a port for your service.
 s.bind((host, port))  # Bind to the port
+game_over = False
+tic_tac_toe_matrix = [[0, 0, 0],
+                      [0, 0, 0]
+                      [0, 0, 0]]
 
 s.listen(5)  # Now wait for client connection.
 while True:
-   c, addr = s.accept()  # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   c.close()  # Close the connection
+    c, addr = s.accept()  # Establish connection with client.
+    print 'Got connection from', addr
+    while not game_over:
+        pass
+
+    c.close()  # Close the connection
