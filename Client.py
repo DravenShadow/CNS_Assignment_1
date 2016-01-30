fe33eee9c798ead8c83f83c45d2b1dc44e978d69
DravenPredator <depree.rowland@gmail.com>
2016-01-29 21:27:16 -0500

    Added comments to program

--- Client.py
+++ Client.py
@@ -1,3 +1,10 @@
+'''
+    Author:  Rowland DePree             Client.py
+
+    This is a client designed to connect to a server file on a different computer.  To do this, you will need to know
+    the server IP address and the port number that you are connecting to.  Changed those in the program in order to connect
+    to the correct server and get back a time stamp of when you connected to the server.
+'''
 import socket
 
 s = socket.socket()
--- Server.py
+++ Server.py
@@ -1,3 +1,13 @@
+'''
+    Author:  Rowland DePree             Server.py
+
+    This is a server file design to allow for a remote connection form a different computer.  To do this, you will need
+    to know your IP address and the port number you wish to use for outside communication.  Change the host name to the
+    IP address and the port number to the port number you are using.  It will then wait for a client program to connect
+    to it and send off the the date and time it connect to the client while print out in the console the address of the
+    computer that it connected from.
+'''
+
 import datetime as dt
 import socket
 
