bcb8e11bef600ea1f0cc93f43aa70e584804fc80
DravenPredator <depree.rowland@gmail.com>
2016-01-30 14:47:49 -0500

    Comment Update

--- Client_Transport_File.py
+++ Client_Transport_File.py
@@ -1,31 +1,42 @@
-'''
+"""
     Author:  Rowland DePree             Client_Transport_Files.py
 
     This is a client design to receive a file from the server.  To do this, you will need to know the server IP address
     and the port number of the server.  Change the host name to the IP address of the server and the port number of the
     server as well.  You will also need to know what kind of file you are receiving.  Then change the file format in the
     name of the file you will be writing to match that file format you are receiving from.
-'''
+"""
 
 import socket
 
-s = socket.socket()
-host = '192.168.1.7'
-port = 12345
 
-s.connect((host, port))
-print 'Connected.'
-file_to_send = r'F:\Download\test.txt'
+def main():
+    """
+    Main program
+    :return:
+    """
+    s = socket.socket()
+    host = '192.168.1.7'
+    port = 12345
 
-f = open(file_to_send, 'rb')
-print 'Sending...'
-l = f.read(1024)
-while (l):
+    s.connect((host, port))
+    print 'Connected.'
+    file_to_send = r'F:\Download\test.txt'
+
+    f = open(file_to_send, 'rb')
     print 'Sending...'
-    s.send(l)
     l = f.read(1024)
-f.close()
-print "Done Sending"
-s.shutdown(socket.SHUT_WR)
-print s.recv(1024)
-s.close
+    while (l):
+        print 'Sending...'
+        s.send(l)
+        l = f.read(1024)
+    f.close()
+    print "Done Sending"
+    s.shutdown(socket.SHUT_WR)
+    print s.recv(1024)
+    s.close
+
+
+# Runs the main program
+if __name__ == '__main__':
+    main()
--- Server_Transport_File.py
+++ Server_Transport_File.py
@@ -1,29 +1,40 @@
-'''
+"""
     Author:  Rowland DePree             Server_Transport_Files.py
 
-    This is a server file design to send a file from the server to the client file.  To do this you will need to know the
-    path of the file location and the port number you are going to use to allow for communication.  Change
-    the host to the IP address, the port to the port number you are using and the file_to_send to the location of the file
-    you wish to send while leaving the r in front of the location.
-'''
+    This is a server file design to send a file from the server to the client file.  To do this you will need to know
+    the path of the file location and the port number you are going to use to allow for communication.  Change
+    the host to the IP address, the port to the port number you are using and the file_to_send to the location of the
+    file you wish to send while leaving the r in front of the location.
+"""
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = ''
-port = 12345
-s.bind((host, port))
-f = open('received_file.txt', 'wb')
-s.listen(5)
-while True:
-    c, addr = s.accept()
-    print 'Got connection from', addr
-    print "Receiving..."
-    l = c.recv(1024)
-    while (l):
+
+def main():
+    """
+    Main program.
+    :return:
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = ''
+    port = 12345
+    s.bind((host, port))
+    f = open('received_file.txt', 'wb')
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        print 'Got connection from', addr
         print "Receiving..."
-        f.write(l)
         l = c.recv(1024)
-    f.close()
-    print "Done Receiving"
-    c.send('Thank you for connecting')
-    c.close()
+        while (l):
+            print "Receiving..."
+            f.write(l)
+            l = c.recv(1024)
+        f.close()
+        print "Done Receiving"
+        c.send('Thank you for connecting')
+        c.close()
+
+
+# Runs the main program
+if __name__ == '__main__':
+    main()
