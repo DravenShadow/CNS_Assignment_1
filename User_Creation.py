1ae0deab694715ba7525d37709e0e4ffd18361b1
DravenPredator <depree.rowland@gmail.com>
2016-01-30 14:51:13 -0500

    Comment Update

--- User_Creation.py
+++ User_Creation.py
@@ -1,16 +1,27 @@
-'''
+"""
     Author:  Rowland DePree             User_Creation.py
 
     This is a program designed to create a new user account on a linux machine.  You will only have to supply a proper
     username and password when prompt to by the program.
-'''
+"""
 import os
 import platform
 
-username = raw_input('Enter in the desired username: ')
-password = raw_input('Enter in the desired password: ')
 
-if platform.system() == 'Linux':
-    os.system('sudo useradd ' + username + ' -m -p ' + password)
-else:
-    print 'Your system is not supported by this program.'
+def main():
+    """
+    Main program
+    :return:
+    """
+    username = raw_input('Enter in the desired username: ')
+    password = raw_input('Enter in the desired password: ')
+
+    if platform.system() == 'Linux':
+        os.system('sudo useradd ' + username + ' -m -p ' + password)
+    else:
+        print 'Your system is not supported by this program.'
+
+
+# Runs the main program
+if __name__ == '__main__':
+    main()
