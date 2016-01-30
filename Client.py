d980b58628b34b682561088961035cd3eab5cde8
DravenPredator <depree.rowland@gmail.com>
2016-01-30 14:50:15 -0500

    Comment Update

--- Client.py
+++ Client.py
@@ -1,16 +1,27 @@
-'''
+"""
     Author:  Rowland DePree             Client.py
 
     This is a client designed to connect to a server file on a different computer.  To do this, you will need to know
-    the server IP address and the port number that you are connecting to.  Changed those in the program in order to connect
-    to the correct server and get back a time stamp of when you connected to the server.
-'''
+    the server IP address and the port number that you are connecting to.  Changed those in the program in order to
+    connect to the correct server and get back a time stamp of when you connected to the server.
+"""
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
 
-s.connect((host, port))
-print s.recv(1024)
-s.close
+def main():
+    """
+    Main program
+    :return:
+    """
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 12345
+
+    s.connect((host, port))
+    print s.recv(1024)
+    s.close
+
+
+# Runs the main program
+if __name__ == '__main__':
+    main()
--- Server.py
+++ Server.py
@@ -1,23 +1,34 @@
-'''
+"""
     Author:  Rowland DePree             Server.py
 
     This is a server file design to allow for a remote connection form a different computer.  To do this, you will need
     to know the port number you will be using for communication with an outside computer.  Change the port number to the
     port number you are using.  It will then wait for a client program to connect to it and send off the the date and
     time it connect to the client while print out in the console the address of the computer that it connected from.
-'''
+"""
 
 import datetime as dt
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = ''
-port = 12345
-s.bind((host, port))
 
-s.listen(5)
-while True:
-    c, addr = s.accept()
-    print 'Got connection from', addr
-    c.send(str(dt.datetime.now()))
-    c.close()
+def main():
+    """
+    Main program
+    :return:
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = ''
+    port = 12345
+    s.bind((host, port))
+
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        print 'Got connection from', addr
+        c.send(str(dt.datetime.now()))
+        c.close()
+
+
+# Rusn the main program
+if __name__ == '__main__':
+    main()
